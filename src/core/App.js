d8467c5765abdce970ba7ff265b8ef9f2b77450d
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-20 16:55:48 +0100

    add Theme provider & GlobalStyle to App.js

--- src/core/App.js
+++ src/core/App.js
@@ -1,6 +1,13 @@
+import {ThemeProvider} from "styled-components";
+import {GlobalStyle} from "../themes/GlobalStyle";
+import {light} from "../themes/theme";
+
 function App() {
   return (
-    <></>
+    <ThemeProvider theme={light}>
+      <GlobalStyle/>
+
+    </ThemeProvider>
   );
 }
 
