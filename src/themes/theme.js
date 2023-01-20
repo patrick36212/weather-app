f57b100a6b57acf6f400818849cb663d812f2f63
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-20 16:56:42 +0100

    create theme with colors for dark & light mode

---
+++ src/themes/theme.js
@@ -0,0 +1,27 @@
+const breakpoints = {
+  mobile: 450,
+  tablet: 768,
+  laptop: 1024
+};
+
+export const light = {
+  breakpoints,
+  colors: {
+    mainFont: "#F7F7F7",
+    secondaryFont: "#1E1E1E",
+    mainBackground: "#4695D1",
+    tileBackground: "#456FD1",
+    secondaryTileBackground: "#304E93"
+  }
+}
+
+export const dark = {
+  breakpoints,
+  colors: {
+    mainFont: "#1E1E1E",
+    secondaryFont: "#F7F7F7",
+    mainBackground: "#28417B",
+    tileBackground: "#315DC4",
+    secondaryTileBackground: "#4B74D2"
+  }
+}
