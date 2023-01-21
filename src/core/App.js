0fa8c1225cd30b9d70c5b860b66b35cc193ec9c0
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-21 16:12:37 +0100

    add components to app.js

--- src/core/App.js
+++ src/core/App.js
@@ -1,12 +1,16 @@
 import {ThemeProvider} from "styled-components";
 import {GlobalStyle} from "../themes/GlobalStyle";
 import {light} from "../themes/theme";
+import Main from "../components/Main";
+import Realtime from "../features/Realtime";
 
 function App() {
   return (
     <ThemeProvider theme={light}>
       <GlobalStyle/>
-
+      <Main>
+        <Realtime/>
+      </Main>
     </ThemeProvider>
   );
 }
