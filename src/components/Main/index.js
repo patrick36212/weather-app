00a336ac95d5c8d575c0f0e683ee85c86c02a747
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-21 16:05:28 +0100

    create main component with basic css

---
+++ src/components/Main/index.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+  height: 100vh;
+  font-family: 'Inter', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0.05em;
+  line-height: 1.5;
+`;
+
+export default Main;
