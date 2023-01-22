d1f92e7858de405628118c6bc9f54fb73d0ba4df
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-22 13:49:34 +0100

    style change for section

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,16 +1,13 @@
 import styled from "styled-components";
 
 export const  Section = styled.section`
-  border-radius: 8px;
-  padding: 12px;
-  max-width: max-content;
-  background: ${({theme}) => theme.colors.secondaryTileBackground};
-  box-shadow: 2px 2px 4px ${({theme}) => theme.colors.tileBackground};
+  padding: 0 0 36px 0;
+  width: 500px;
 `;
 
 export const SectionTitle = styled.h2`
+  padding: 14px 0 36px 10px;
   margin: 0;
   font-weight: 900;
   font-size: 36px;
-  text-align: center;
 `;
