3a39fa3a9454186ef44a81327f0be344bebfe095
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-23 15:33:55 +0100

    add display: grid to main for all components

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 const Main = styled.main`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 16px;
+  padding: 20px;
   height: 100vh;
   font-family: 'Inter', sans-serif;
   font-size: 16px;
   font-weight: 400;
   letter-spacing: 0.05em;
   line-height: 1.5;
+  display: grid;
+  grid-template-columns: max-content 1fr;
+  gap: 20px;
 `;
 
 export default Main;
