4626845158564defa8af3fc902fe13c794ca23a7
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-23 15:34:39 +0100

    add Aside to App

--- src/core/App.js
+++ src/core/App.js
@@ -3,12 +3,14 @@ import {GlobalStyle} from "../themes/GlobalStyle";
 import {light} from "../themes/theme";
 import Main from "../components/Main";
 import Realtime from "../features/Realtime";
+import Aside from "../features/Aside";
 
 function App() {
   return (
     <ThemeProvider theme={light}>
       <GlobalStyle/>
       <Main>
+        <Aside/>
         <Realtime/>
       </Main>
     </ThemeProvider>
