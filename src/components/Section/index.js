e890fff5c9551d9986187ee6efa87bf7a2c9287e
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-23 15:34:14 +0100

    add section style for dashboard

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,8 +1,16 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export const  Section = styled.section`
   padding: 0 0 36px 0;
   width: 500px;
+  
+  ${({dashboard}) => dashboard && css`
+    width: 100%;
+    padding: 32px 20px;
+    border-radius: 20px;
+    background: ${({theme}) => theme.colors.sectionBackground};
+    box-shadow: 4px 4px 4px 0 ${({theme}) => theme.colors.shadow};
+  `}
 `;
 
 export const SectionTitle = styled.h2`
@@ -10,4 +18,8 @@ export const SectionTitle = styled.h2`
   margin: 0;
   font-weight: 900;
   font-size: 36px;
+  
+  ${({transparent}) => transparent && css`
+    color: ${({theme}) => theme.colors.secondaryFont};
+  `}
 `;
