8cf92a67e8ee2a361832f37b559c7e2658dbc0d8
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-23 15:36:01 +0100

    change realtime component to be a dashboard with more views

--- src/features/Realtime/index.js
+++ src/features/Realtime/index.js
@@ -1,43 +1,10 @@
-import {Section, SectionTitle} from "../../components/Section";
-import Tile from "./Tile";
-import {TilesWrapper} from "./Tile/styled";
+import {Section} from "../../components/Section";
+import RealTimeTiles from "./RealTimeTiles";
 
 const Realtime = () => {
   return (
-    <Section>
-      <SectionTitle>Capital cities</SectionTitle>
-      <TilesWrapper>
-        <Tile
-          city={"Warsaw"}
-          degrees={"+7℃"}
-          air={"Fair"}
-        />
-        <Tile
-          city={"Warsaw"}
-          degrees={"+7℃"}
-          air={"Fair"}
-        />
-        <Tile
-          city={"Warsaw"}
-          degrees={"+7℃"}
-          air={"Fair"}
-        />
-        <Tile
-          city={"Warsaw"}
-          degrees={"+7℃"}
-          air={"Fair"}
-        />
-        <Tile
-          city={"Warsaw"}
-          degrees={"+7℃"}
-          air={"Fair"}
-        />
-        <Tile
-          city={"Warsaw"}
-          degrees={"+7℃"}
-          air={"Fair"}
-        />
-      </TilesWrapper>
+    <Section dashboard>
+      <RealTimeTiles/>
     </Section>
   )
 }
