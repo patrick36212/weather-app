fd94c8ef47cf38006c0709b04df0dae00be430c6
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-24 19:56:07 +0100

    create header component with actual datetime

---
+++ src/features/Header/index.js
@@ -0,0 +1,31 @@
+import {HeaderDay, HeaderInfo, HeaderInfoWrapper, HeaderWrapper} from "./styled";
+import {useEffect, useState} from "react";
+
+const Header = () => {
+
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, []);
+
+  const day = new Intl.DateTimeFormat("en", {weekday: "long"}).format(date);
+
+  return(
+    <HeaderWrapper>
+      <HeaderInfoWrapper>
+        <HeaderDay>{day}</HeaderDay>
+        <HeaderInfo>{date.toLocaleTimeString()}</HeaderInfo>
+        <HeaderInfo>{date.toLocaleDateString()}</HeaderInfo>
+      </HeaderInfoWrapper>
+    </HeaderWrapper>
+  )
+}
+
+export default Header;
---
+++ src/features/Header/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  margin: 0 100px;
+`;
+
+export const HeaderInfoWrapper = styled.div`
+  
+`;
+
+export const HeaderDay = styled.h1`
+  font-size: 36px;
+  font-weight: 900;
+  line-height: 44px;
+  margin: 0;
+`;
+
+export const HeaderInfo = styled.p`
+  font-size: 24px;
+  line-height: 30px;
+  margin: 10px 0;
+`;
