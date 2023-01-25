375f55cc1fc07b73623fc69cc9d3a934ddf50d08
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-25 17:58:48 +0100

    change styled Components name, make code more readable

--- src/features/Realtime/RealTimeInfo/styled.js
+++ src/features/Realtime/RealTimeInfo/styled.js
@@ -1,32 +1,60 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
+import {SunriseFill, SunsetFill} from "@styled-icons/bootstrap"
 
-export const RIWrapper = styled.article`
-  display: flex;
-  flex-direction: column;
-  max-width: max-content;
+export const MainWrapper = styled.article`
+  display: grid;
+  grid-template-columns: repeat(3, max-content);
+  align-items: center;
+  gap: 55px;
   color: ${({theme}) => theme.colors.secondaryColor};
 `;
 
-export const RIAdditionalInfoWrapper = styled.article`
+export const AdditionalWrapper = styled.article`
+  display: grid;
+  grid-auto-rows: max-content;
+  gap: 14px;
+`;
+
+export const InfoWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
-export const RITitle = styled.h2`
-  font-size:24px;
+
+export const IconInfoWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, max-content);
+  gap: 12px;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 24px;
   font-weight: 600;
   line-height: 28px;
   margin: 6px 0;
+  
+  ${({centerTitle}) => centerTitle && css`
+    text-align: center;
+  `}
 `;
 
-export const RIInfo = styled.p`
+export const Info = styled.p`
   font-size: 32px;
   font-weight: 600;
   line-height: 38px;
-  margin: 10px 0;
+  margin: 0;
 `;
 
-export const RIAdditionalInfo = styled.span`
+export const AdditionalInfo = styled.span`
   font-size: 16px;
   line-height: 20px;
   margin: 6px 0 0 0;
+`;
+
+export const SunriseIcon = styled(SunriseFill)`
+  width: 48px;
+`;
+
+export const SunsetIcon = styled(SunsetFill)`
+  width: 48px;
 `;
