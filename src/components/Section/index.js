b0495ad14f664a7e6e4223540579fdba81f8808d
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-26 18:56:22 +0100

    add width 100% and innerSection prop

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,8 +1,14 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export const  Section = styled.section`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
   max-width: ${({theme}) => theme.breakpoints.laptop}px;
+  width: 100%;
+  
+  ${({innerSection}) => innerSection && css`
+    justify-content: center;
+    height: 100%;
+  `}
 `;
