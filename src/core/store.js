4734118b5741a7a67cedf7fa75eb3f919047f5be
Patryk Krawczyk <patrick36212@gmail.com>
2023-01-30 20:00:04 +0100

    implement saga

--- src/core/store.js
+++ src/core/store.js
@@ -1,10 +1,18 @@
 import {configureStore} from "@reduxjs/toolkit";
+import createSagaMiddleware  from "redux-saga";
+import rootSaga from "./rootSaga";
+import coordinatesReducer from "../components/coordinates/coordinatesSlice";
 import realTimeReducer from "../features/Realtime/realTimeSlice";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
+    coordinates: coordinatesReducer,
     realTime: realTimeReducer,
-  }
+  },
+  middleware: [sagaMiddleware]
 });
 
+sagaMiddleware.run(rootSaga);
 export default store;
