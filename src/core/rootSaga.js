2e0b8099c7bc49739c286756f5bdc182e2bb8ef3
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-04 13:41:29 +0100

    add realtime saga to root

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,8 +1,10 @@
 import {all} from "redux-saga/effects";
 import {watchFetchCoordinates} from "../components/coordinates/coordinatesSaga";
+import {watchFetchRealTime} from "../features/Realtime/realTimeSaga";
 
 export default function* rootSaga() {
   yield all([
     watchFetchCoordinates(),
+    watchFetchRealTime(),
   ])
 };
