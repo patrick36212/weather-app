2e386429022db7c0eec6088db81b389a2596833a
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-06 19:42:42 +0100

    delete store and implement React-Query

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './core/App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-import store from "./core/store";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries: {
+      staleTime: 5000,
+      retry: 0,
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools/>
       <App/>
-    </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
