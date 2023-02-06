42fd839dbf8d97c95e68ceb2c67571698dcfced7
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-06 19:43:29 +0100

    delete redux, implement React-Query

--- src/components/Header/Search/index.js
+++ src/components/Header/Search/index.js
@@ -9,42 +9,45 @@ import {
   SearchInputWrapper,
   SearchWrapper
 } from "./styled";
-import {useDispatch, useSelector} from "react-redux";
-import {selectCoordinates, selectQuery, setQuery} from "../../coordinates/coordinatesSlice";
-import {setCoordinates} from "../../../features/Realtime/realTimeSlice";
+import {useState} from "react";
+import {useQuery} from "react-query";
+import {getSearchData} from "./getSearchData";
+import useDebounce from "./useDebounce";
 
 const Search = () => {
-  const dispatch = useDispatch();
-  const query = useSelector(selectQuery)
-  const coordinates = useSelector(selectCoordinates);
+  const [query, setQuery] = useState("");
+
+  const debouncedQuery = useDebounce(query, 500)
+
+  const {data} = useQuery(
+    ["autocomplete", debouncedQuery],
+    () => {
+      if(!query) {
+        return null;
+      } else {
+        return getSearchData(debouncedQuery);
+      }
+    }
+  );
 
   return (
     <SearchWrapper>
       <SearchInputWrapper>
         <SearchIcon/>
         <SearchInput
-          onChange={({target}) => dispatch(setQuery(target.value))}
+          onChange={({target}) => setQuery(target.value)}
           placeholder="Search by city name"
           value={query || ""}
         />
       </SearchInputWrapper>
-      {query &&
+      {data &&
         <SearchDropdownWrapper>
           <SearchDropdownInfoList>
-            {coordinates.map(city => (
-              <SearchDropdownInfoItem key={city.lat}>
-                <SearchDropdownInfoButton
-                  onClick={() => {
-                    dispatch(setCoordinates({
-                      city: city.name,
-                      lat: city.lat,
-                      lon: city.lon
-                    }));
-                    dispatch(setQuery(""));
-                  }}
-                >
-                  <SearchDropdownInfo cityName>{city.name}, {city.country}</SearchDropdownInfo>
-                  <SearchDropdownInfo>lat: {city.lat.toFixed(2)} lon: {city.lon.toFixed(2)}</SearchDropdownInfo>
+            {data.map(autocomplete => (
+              <SearchDropdownInfoItem key={autocomplete.id}>
+                <SearchDropdownInfoButton>
+                  <SearchDropdownInfo cityName>{autocomplete.name}, {autocomplete.country}</SearchDropdownInfo>
+                  <SearchDropdownInfo>lat: {autocomplete.lat} lon: {autocomplete.lon}</SearchDropdownInfo>
                 </SearchDropdownInfoButton>
               </SearchDropdownInfoItem>
             ))}
