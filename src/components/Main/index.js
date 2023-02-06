1b4be94bfb7575d96393eaf07d9d61738176677c
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-06 19:52:47 +0100

    change styles for this components structure

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,14 @@
 import styled from "styled-components";
 
 const Main = styled.main`
-  padding: 20px;
-  height: 100vh;
+  padding: 0 20px 20px;
+  height: calc(100vh - 120px);
   font-family: 'Inter', sans-serif;
   font-size: 16px;
   font-weight: 400;
   letter-spacing: 0.05em;
   display: grid;
-  grid-template-rows: max-content 1fr;
-  gap: 20px;
+  grid-auto-rows: max-content;
 `;
 
 export default Main;
