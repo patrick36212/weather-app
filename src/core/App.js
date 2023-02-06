3202058577db83348aebaa668d2013541422e9d6
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-06 19:44:53 +0100

    move header to top, delete unnecessary code

--- src/core/App.js
+++ src/core/App.js
@@ -1,22 +1,17 @@
 import {ThemeProvider} from "styled-components";
 import {GlobalStyle} from "../themes/GlobalStyle";
 import {light} from "../themes/theme";
-import {Section} from "../components/Section";
 import Main from "../components/Main";
-import Realtime from "../features/Realtime";
-import DateComponent from "../components/DateComponent";
 import Header from "../components/Header";
+import Realtime from "../features/Realtime";
 
 function App() {
   return (
     <ThemeProvider theme={light}>
       <GlobalStyle/>
+      <Header/>
       <Main>
-        <Header/>
-        <Section>
-          <DateComponent/>
-          <Realtime/>
-        </Section>
+        <Realtime/>
       </Main>
     </ThemeProvider>
   );
