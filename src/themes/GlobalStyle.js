bdf2d8854b658d4cc1a1186aa9ad121e0bc67329
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-08 17:46:01 +0100

    add wordwrap and transfer 100vh to body component from main

--- src/themes/GlobalStyle.js
+++ src/themes/GlobalStyle.js
@@ -4,6 +4,8 @@ import normalize from "styled-normalize";
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
   body {
+    word-wrap: anywhere;
+    height: 100vh;
     box-sizing: border-box;
     color: ${({theme}) => theme.colors.mainFont};
     background: linear-gradient(67.1deg, ${({theme}) => theme.colors.mainColor} 0%, transparent 100%);
