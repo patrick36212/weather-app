9df8feb3054646d989066cb55c705669ccd92a1f
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-09 17:17:35 +0100

    create Loading component

---
+++ src/components/StatusInfo/Loading/index.js
@@ -0,0 +1,12 @@
+import {StatusParagraph, StatusTitle, StatusWrapper} from "../styled";
+import {LoaderIcon} from "./styled";
+
+const Loading = () => (
+  <StatusWrapper>
+    <StatusTitle>Checking weather</StatusTitle>
+    <StatusParagraph>Please wait...</StatusParagraph>
+    <LoaderIcon/>
+  </StatusWrapper>
+);
+
+export default Loading;
---
+++ src/components/StatusInfo/Loading/styled.js
@@ -0,0 +1,15 @@
+import {Sun} from "@styled-icons/bootstrap/Sun";
+import styled, {keyframes} from "styled-components";
+
+const rotation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+export const LoaderIcon = styled(Sun)`
+  width: 100px;
+  animation: ${rotation} 5s linear infinite;
+`;
