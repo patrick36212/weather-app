445e627006333dd608ec1de67deb8a43fea18107
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-09 17:18:07 +0100

    styled components for error and loading

---
+++ src/components/StatusInfo/styled.js
@@ -0,0 +1,25 @@
+import styled, {css} from "styled-components";
+
+export const StatusWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: ${({theme}) => theme.colors.secondaryColor};
+`;
+
+export const StatusTitle = styled.h3`
+  font-weight: 900;
+  font-size: 36px;
+  margin: 8px 0;
+`;
+
+export const StatusParagraph = styled.p`
+  font-size: 24px;
+  font-weight: 600;
+  margin: 24px 0 60px;
+  
+  ${({error}) => error && css`
+    margin: 24px 0;
+  `}
+`;
