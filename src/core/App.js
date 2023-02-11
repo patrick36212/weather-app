376b45210c282ffc274e1d40451beed5455cf087
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-11 21:31:25 +0100

    delete props & state from app component, move DateComponent to App

--- src/core/App.js
+++ src/core/App.js
@@ -1,23 +1,19 @@
-import {ThemeProvider} from "styled-components";
-import {GlobalStyle} from "../themes/GlobalStyle";
-import {light} from "../themes/theme";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../themes/GlobalStyle";
+import { light } from "../themes/theme";
 import Main from "../components/Main";
 import Header from "../components/Header";
 import Realtime from "../features/Realtime";
-import {useState} from "react";
+import DateComponent from "../components/DateComponent";
 
 function App() {
-  const [coordinates, setCoordinates] = useState({
-    lat: null,
-    lon: null
-  })
-
   return (
     <ThemeProvider theme={light}>
-      <GlobalStyle/>
-      <Header setCoordinates={setCoordinates}/>
+      <GlobalStyle />
+      <Header />
       <Main>
-        <Realtime coordinates={coordinates}/>
+        <DateComponent />
+        <Realtime />
       </Main>
     </ThemeProvider>
   );
