879fceaf04ff0df787484b0de2a91d091e5d1ede
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-11 21:32:15 +0100

    change date format to be equal with api

--- src/components/DateComponent/index.js
+++ src/components/DateComponent/index.js
@@ -1,19 +1,23 @@
-import {DateInfo, DateTitle, DateWrapper} from "./styled";
+import { DateInfo, DateTitle, DateWrapper } from "./styled";
 
 const DateComponent = () => {
   const timestamp = new Date();
-  const day = new Intl.DateTimeFormat("en", {weekday: "long"}).format(timestamp);
-  const fullDate = timestamp.toLocaleDateString();
+  const day = new Intl.DateTimeFormat("en", { weekday: "long" }).format(
+    timestamp
+  );
+  const fullDate = timestamp
+    .toLocaleDateString()
+    .split(".")
+    .reverse()
+    .join("-");
 
   return (
     <DateWrapper>
       <DateTitle>Today is</DateTitle>
       <DateInfo>{day}</DateInfo>
-      <DateInfo additional>
-        {fullDate}
-      </DateInfo>
+      <DateInfo additional>{fullDate}</DateInfo>
     </DateWrapper>
-  )
+  );
 };
 
 export default DateComponent;
