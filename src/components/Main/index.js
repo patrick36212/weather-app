8dceaa07357a6a5485454d0336dc2611f0127e67
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-11 21:33:28 +0100

    style change

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 const Main = styled.main`
-  padding: 0 20px;
   font-family: 'Inter', sans-serif;
   font-size: 16px;
   font-weight: 400;
   letter-spacing: 0.05em;
-  display: grid;
-  grid-auto-rows: max-content;
+  max-width: 1024px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
 `;
 
 export default Main;
