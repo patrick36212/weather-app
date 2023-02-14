626ca809af0b388f5b53cc465e67b1a523b8ba11
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-14 18:46:54 +0100

    create state for themeSwitch

--- src/core/App.js
+++ src/core/App.js
@@ -1,16 +1,19 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../themes/GlobalStyle";
-import { light } from "../themes/theme";
+import { dark, light } from "../themes/theme";
 import Main from "../components/Main";
 import Header from "../components/Header";
 import Realtime from "../features/Realtime";
 import DateComponent from "../components/DateComponent";
+import { useState } from "react";
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
   return (
-    <ThemeProvider theme={light}>
+    <ThemeProvider theme={isDarkTheme ? dark : light}>
       <GlobalStyle />
-      <Header />
+      <Header isDarkTheme={isDarkTheme} setIsDarkTheme={setIsDarkTheme} />
       <Main>
         <DateComponent />
         <Realtime />
