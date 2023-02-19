a39ceafd2d27466404b5fb605709d69be221afa4
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-19 17:04:14 +0100

    adjust css, add refresh and delete button

--- src/components/InfoTile/styled.js
+++ src/components/InfoTile/styled.js
@@ -1,12 +1,20 @@
 import styled, { css } from "styled-components";
+import { Delete } from "@styled-icons/typicons/Delete";
+import { Refresh } from "@styled-icons/material-sharp/Refresh";
 
 export const MainWrapper = styled.article`
-  display: flex;
+  position: relative;
+  display: inherit;
   flex-direction: column;
+  justify-content: center;
+  flex-grow: 1;
   padding: 20px;
-  max-width: 100%;
-  max-height: 100%;
+  width: 100%;
   color: ${({ theme }) => theme.colors.secondaryFont};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    padding: 10px;
+  }
 `;
 
 export const Title = styled.h2`
@@ -15,6 +23,7 @@ export const Title = styled.h2`
   margin: 0;
   padding: 0 0 6px 0;
   border-bottom: 2px solid;
+  max-width: 85%;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     font-size: 18px;
@@ -27,16 +36,21 @@ export const AdditionalInfo = styled.span`
   line-height: 20px;
   margin: 6px 0 0 0;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    font-size: 11px;
-  }
-
   ${({ bold }) =>
     bold &&
     css`
       font-weight: 600;
       text-align: center;
-    `}
+    `};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 14px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileS}px) {
+    font-size: 10px;
+    line-height: 16px;
+  }
 `;
 
 export const IconInfoWrapper = styled.div`
@@ -45,7 +59,6 @@ export const IconInfoWrapper = styled.div`
   justify-items: center;
   align-items: center;
   gap: 0 4px;
-  max-width: 90%;
 `;
 
 export const Icon = styled.div`
@@ -63,12 +76,13 @@ export const Icon = styled.div`
 `;
 
 export const Info = styled.p`
-  font-size: 42px;
+  font-size: 28px;
   font-weight: 700;
   margin: 0;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    font-size: 28px;
+    font-size: 18px;
+    font-weight: 600;
   }
 `;
 
@@ -78,6 +92,7 @@ export const InfoWrapper = styled.div`
   align-items: center;
   margin: 12px 0;
   grid-template-columns: repeat(2, 1fr);
+  gap: 6px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     margin: 0;
@@ -93,5 +108,42 @@ export const AdditionsInfoWrapper = styled.div`
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     border-left: 1px solid;
+  }
+`;
+
+export const ControlButtonsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+export const RefreshButton = styled(Refresh)`
+  cursor: pointer;
+  width: 30px;
+  transition: 0.7s ease-in-out;
+
+  &:hover {
+    transform: rotate(360deg);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    width: 20px;
+  }
+`;
+
+export const DeleteButton = styled(Delete)`
+  width: 30px;
+  cursor: pointer;
+  transition: 0.3s ease-out;
+
+  &:hover {
+    fill: crimson;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    width: 20px;
   }
 `;
