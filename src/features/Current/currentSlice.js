85f7b44c39b89f55fdd564f5d637f052f2f6ac93
Patryk Krawczyk <patrick36212@gmail.com>
2023-02-23 17:07:11 +0100

    add cityCoordinates state and reducer, working on update reducer

--- src/features/Current/currentSlice.js
+++ src/features/Current/currentSlice.js
@@ -6,6 +6,7 @@ const currentSlice = createSlice({
     cityList: [],
     isSearchActive: false,
     coordinates: null,
+    cityCoordinates: null,
   },
   reducers: {
     setCityList: ({ cityList }, { payload: city }) => {
@@ -15,8 +16,18 @@ const currentSlice = createSlice({
       const cityIndex = cityList.findIndex(({ id }) => id === cityId);
       cityList.splice(cityIndex, 1);
     },
+    updateCityInCityList: (
+      { cityList },
+      { payload: cityId, updatedCityData }
+    ) => {
+      const cityIndex = cityList.findIndex(({ id }) => id === cityId);
+      // fn for update particular object data
+    },
     setCoordinates: (state, { payload: coordinates }) => {
       state.coordinates = coordinates;
+    },
+    setCityCoordinates: (state, { payload: cityCoordinates }) => {
+      state.cityCoordinates = cityCoordinates;
     },
     toggleSearchActive: (state) => {
       state.isSearchActive = !state.isSearchActive;
@@ -28,7 +39,9 @@ export const {
   setCityList,
   removeCityFromCityList,
   setCoordinates,
+  setCityCoordinates,
   toggleSearchActive,
+  updateCityInCityList,
 } = currentSlice.actions;
 
 const selectCurrentState = (state) => state.current;
@@ -36,6 +49,8 @@ const selectCurrentState = (state) => state.current;
 export const selectCityList = (state) => selectCurrentState(state).cityList;
 export const selectCoordinates = (state) =>
   selectCurrentState(state).coordinates;
+export const selectCityCoordinates = (state) =>
+  selectCurrentState(state).cityCoordinates;
 export const selectIsSearchActive = (state) =>
   selectCurrentState(state).isSearchActive;
 
