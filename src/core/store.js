170bc260e59d4e36a2fed4ed65355cd0c34e67df
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-10 06:09:36 +0100

    add theme reducer to store

--- src/core/store.js
+++ src/core/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import currentReducer from "../features/Current/currentSlice";
+import themeReducer from "../components/Header/ThemeSwitch/themeSlice";
 
 const store = configureStore({
   reducer: {
     current: currentReducer,
+    theme: themeReducer,
   },
 });
 
