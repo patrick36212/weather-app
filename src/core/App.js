ecb82d5396411e3c83549d55cb00b0328032e20e
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-10 06:10:04 +0100

    add theme state for dark/light mode

--- src/core/App.js
+++ src/core/App.js
@@ -4,10 +4,14 @@ import { GlobalStyle } from "../themes/GlobalStyle";
 import { dark, light } from "../themes/theme";
 import { router } from "./routes";
 import { RouterProvider } from "react-router";
+import { useSelector } from "react-redux";
+import { selectIsDarkThemeOn } from "../components/Header/ThemeSwitch/themeSlice";
 
 function App() {
+  const isDarkThemeOn = useSelector(selectIsDarkThemeOn);
+
   return (
-    <ThemeProvider theme={dark}>
+    <ThemeProvider theme={isDarkThemeOn ? dark : light}>
       <GlobalStyle />
       <RouterProvider router={router} />
     </ThemeProvider>
