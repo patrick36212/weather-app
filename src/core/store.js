fd0217ff4b235c082dec926236410036243aa408
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-13 19:47:00 +0100

    implement searchReducer to store

--- src/core/store.js
+++ src/core/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import searchReducer from "../components/Search/searchSlice";
 import currentReducer from "../features/Current/currentSlice";
 import themeReducer from "../components/Header/ThemeSwitch/themeSlice";
 
 const store = configureStore({
   reducer: {
+    search: searchReducer,
     current: currentReducer,
     theme: themeReducer,
   },
